da428378df3b3d88ec385bc05330c3dfbf3cd5dc
Arignir <github@sigint.sh>
2022-10-02 14:57:09 +0200

    Fix `openbus-basic.gba`.

--- source/openbus/basic.c
+++ source/openbus/basic.c
@@ -3,7 +3,10 @@
 #include <gba_interrupt.h>
 #include <stdio.h>
 
-int main(void) {
+IWRAM_CODE
+int
+main(void)
+{
     irqInit();
     consoleDemoInit();
 
@@ -14,18 +17,18 @@ int main(void) {
     VBlankIntrWait();
 
     uint32_t values[12][2] = {
-        { 0xe3a02004, *(u32 *)0x0 },
-        { 0x00002004, *(u16 *)0x0 },
-        { 0x00000004, *(u8 *)0x0 },
-        { 0x04e3a020, *(u32 *)0x1 },
-        { 0x04000020, *(u16 *)0x1 },
-        { 0x00000020, *(u8 *)0x1 },
-        { 0x2004e3a0, *(u32 *)0x2 },
-        { 0x0000e3a0, *(u16 *)0x2 },
-        { 0x000000a0, *(u8 *)0x2 },
-        { 0xa02004e3, *(u32 *)0x3 },
-        { 0xa00000e3, *(u16 *)0x3 },
-        { 0x000000e3, *(u8 *)0x3 },
+        { 0xe3a02004, *(vu32 *)0x0 },
+        { 0x00002004, *(vu16 *)0x0 },
+        { 0x00000004, *(vu8 *)0x0 },
+        { 0x04e3a020, *(vu32 *)0x1 },
+        { 0x00000020, *(vu16 *)0x1 },
+        { 0x00000020, *(vu8 *)0x1 },
+        { 0x2004e3a0, *(vu32 *)0x2 },
+        { 0x0000e3a0, *(vu16 *)0x2 },
+        { 0x000000a0, *(vu8 *)0x2 },
+        { 0xa02004e3, *(vu32 *)0x3 },
+        { 0x000000e3, *(vu16 *)0x3 },
+        { 0x000000e3, *(vu8 *)0x3 },
     };
 
     for (int i = 0; i < 12; ++i) {
@@ -33,7 +36,11 @@ int main(void) {
 
         success = values[i][0] == values[i][1];
 
-        printf("Test %i: %s\n", i, success ? "success" : "fail");
+        if (success) {
+            printf("%i: success\n", i + 1);
+        } else {
+            printf("%i: fail %08lx != %08lx\n", i + 1, values[i][0], values[i][1]);
+        }
     }
 
     while (true) {
