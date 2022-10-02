3528cb6f09541ae0bf591e563b2f5a88ef00c670
Arignir <github@sigint.sh>
2022-10-02 16:00:33 +0200

    Add license headers to all code files.

--- source/openbus/unaligned-access.c
+++ source/openbus/unaligned-access.c
@@ -1,3 +1,12 @@
+/******************************************************************************\
+**
+**  This file is part of the Hades GBA Emulator, and is made available under
+**  the terms of the GNU General Public License version 2.
+**
+**  Copyright (C) 2021-2022 - The Hades Authors
+**
+\******************************************************************************/
+
 #include <gba_console.h>
 #include <gba_systemcalls.h>
 #include <gba_interrupt.h>
