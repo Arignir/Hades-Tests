cd13366c2756e8c21b4dfcaabbf717c0ab86091b
Arignir <tigearth@hotmail.fr>
2022-12-28 03:53:13 +0100

    Add a test to `dma-start-delay.gba`.

--- source/dma/start-delay.c
+++ source/dma/start-delay.c
@@ -83,7 +83,7 @@ uint16_t nb_test_fail = 0;
     } \
 
 /*
-** Start DMA0 with TM0CNT as the source address.
+** Run DMA0 with TM0CNT as the source address.
 */
 NEW_TEST(1, 2,  {
     if (iwram) {
@@ -110,6 +110,39 @@ NEW_TEST(1, 2,  {
     REG_DMA0CNT = 0;
 })
 
+/*
+** Run DMA0 with TM0CNT as the source address, but immediately stop it.
+*/
+NEW_TEST(2, 3,  {
+    if (iwram) {
+        expected[0] = 0x0016;
+        expected[1] = 0x0021;
+        expected[2] = 0x0033;
+    } else {
+        expected[0] = 0x0073;
+        expected[1] = 0x00A7;
+        expected[2] = 0x00FE;
+    }
+
+    REG_TM0CNT_H = 0;
+    REG_TM0CNT_L = 0;
+    REG_TM0CNT_H = TIMER_START;
+
+    REG_DMA0SAD = (u32)&REG_TM0CNT_L;
+    REG_DMA0DAD = (u32)&samples[0];
+    REG_DMA0CNT = DMA_ENABLE | DMA16 | DMA_IMMEDIATE | 1;
+    REG_DMA0CNT = 0;
+
+    samples[1] = REG_TM0CNT_L;
+
+    while (REG_DMA0CNT & DMA_ENABLE);
+
+    samples[2] = REG_TM0CNT_L;
+
+    REG_TM0CNT_H = 0;
+    REG_DMA0CNT = 0;
+})
+
 IWRAM_CODE
 int
 main(void)
@@ -124,8 +157,10 @@ main(void)
     VBlankIntrWait();
 
     test_01_rom();
-
     test_01_iwram();
+
+    test_02_rom();
+    test_02_iwram();
 
     printf("\n");
     printf("Total: %u/%u\n", nb_test_pass, nb_test_pass + nb_test_fail);
