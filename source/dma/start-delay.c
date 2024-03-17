80b6e5fe508e7ee5ca26d01b8f0b09354fbdc45d
Arignir <github@sigint.sh>
2024-03-17 17:14:06 +0100

    Update the DMA startup-delay test.

--- source/dma/start-delay.c
+++ source/dma/start-delay.c
@@ -14,86 +14,93 @@
 #include <gba_systemcalls.h>
 #include <stdio.h>
 
+#define REG_WAITCNT         *(vu32*)(REG_BASE + 0x204)
+#define WAITCNT_PREFETCH    (1 << 14)
+
 uint16_t nb_test_pass = 0;
 uint16_t nb_test_fail = 0;
 
-/*
-** Ideas to explore:
-**   - Disable a DMA before it starts
-**   - Enable a DMA during a very long insn and see
-**      if is triggered during the previous instruction.
-*/
+enum test_kind {
+    TEST_KIND_IWRAM,
+    TEST_KIND_EWRAM,
+    TEST_KIND_ROM_WITH_PREFETCH,
+    TEST_KIND_ROM_WITHOUT_PREFETCH,
 
-#define NEW_TEST(_idx, _samples_nb, _code) \
+    TEST_KIND_MAX,
+};
+
+#define TEST_INNER_FN(_kind, _kind_str, _idx, _test_results, _code) \
+    { \
+        u16 samples[sizeof(_test_results[0]) / sizeof(uint16_t)]; \
+        u32 samples_count; \
+        u32 i; \
+        \
+        samples_count = sizeof(_test_results[0]) / sizeof(uint16_t); \
+        \
+        if ((_kind) == TEST_KIND_ROM_WITH_PREFETCH) { \
+            REG_WAITCNT |= WAITCNT_PREFETCH; \
+        } else { \
+            REG_WAITCNT &= ~WAITCNT_PREFETCH; \
+        } \
+        \
+        for (i = 0; i < samples_count; ++i) { \
+            samples[i] = 0xDEAD; \
+        } \
+        \
+        _code; \
+        \
+        for (i = 0; i < samples_count; ++i) { \
+            if (samples[i] != _test_results[(_kind)][i]) { \
+                printf(_kind_str " %i: FAIL 0x%04X != 0x%04X", (_idx), samples[i], _test_results[(_kind)][i]); \
+                nb_test_fail += 1; \
+                return ; \
+            } \
+        } \
+        printf(_kind_str " %i: PASS\n", (_idx)); \
+        nb_test_pass += 1; \
+    }
+
+#define NEW_TEST(_idx, _test_results, _code) \
     IWRAM_CODE \
     static \
     void \
     test_0##_idx##_iwram(void) \
     { \
-        u16 samples[_samples_nb]; \
-        u16 expected[_samples_nb]; \
-        bool iwram __unused; \
-        int i; \
-        \
-        for (i = 0; i < _samples_nb; ++i) { \
-            samples[i] = 0xDEAD; \
-        } \
-        \
-        iwram = true; \
-        \
-        _code; \
-        \
-        for (i = 0; i < _samples_nb; ++i) { \
-            if (samples[i] != expected[i]) { \
-                printf("IWRAM %i: FAIL 0x%04X != 0x%04X", (_idx), samples[i], expected[i]); \
-                nb_test_fail += 1; \
-                return ; \
-            } \
-        } \
-        printf("IWRAM %i: PASS\n", (_idx)); \
-        nb_test_pass += 1; \
+        TEST_INNER_FN(TEST_KIND_IWRAM, "IWRAM", (_idx), (_test_results), _code); \
+    } \
+    \
+    EWRAM_CODE \
+    static \
+    void \
+    test_0##_idx##_ewram(void) \
+    { \
+        TEST_INNER_FN(TEST_KIND_EWRAM, "EWRAM", (_idx), (_test_results), _code); \
     } \
     \
     static \
     void \
-    test_0##_idx##_rom(void) \
+    test_0##_idx##_rom_with_prefetch(void) \
     { \
-        u16 samples[_samples_nb]; \
-        u16 expected[_samples_nb]; \
-        bool iwram __unused; \
-        int i; \
-        \
-        for (i = 0; i < _samples_nb; ++i) { \
-            samples[i] = 0xDEAD; \
-        } \
-        \
-        iwram = false; \
-        \
-        _code; \
-        \
-        for (i = 0; i < _samples_nb; ++i) { \
-            if (samples[i] != expected[i]) { \
-                printf("ROM   %i: FAIL 0x%04X != 0x%04X", (_idx), samples[i], expected[i]); \
-                nb_test_fail += 1; \
-                return ; \
-            } \
-        } \
-        printf("ROM   %i: PASS\n", (_idx)); \
-        nb_test_pass += 1; \
+        TEST_INNER_FN(TEST_KIND_ROM_WITH_PREFETCH, "ROM P", (_idx), (_test_results), _code); \
+    } \
+    \
+    static \
+    void \
+    test_0##_idx##_rom_without_prefetch(void) \
+    { \
+        TEST_INNER_FN(TEST_KIND_ROM_WITHOUT_PREFETCH, "ROM  ", (_idx), (_test_results), _code); \
     } \
 
 /*
 ** Run DMA0 with TM0CNT as the source address.
 */
-NEW_TEST(1, 2,  {
-    if (iwram) {
-        expected[0] = 0x0016;
-        expected[1] = 0x0022;
-    } else {
-        expected[0] = 0x0073;
-        expected[1] = 0x00AE;
-    }
-
+u16 TEST_01_RESULTS[TEST_KIND_MAX][2] = {
+    [TEST_KIND_IWRAM]                   = { 0x0016, 0x0022 },
+    [TEST_KIND_EWRAM]                   = { 0x005B, 0x008C },
+    [TEST_KIND_ROM_WITH_PREFETCH]       = { 0x0067, 0x009E },
+    [TEST_KIND_ROM_WITHOUT_PREFETCH]    = { 0x0073, 0x00AE },
+};
+NEW_TEST(1, TEST_01_RESULTS, {
     REG_TM0CNT_H = 0;
     REG_TM0CNT_L = 0;
     REG_TM0CNT_H = TIMER_START;
@@ -113,17 +120,13 @@ NEW_TEST(1, 2,  {
 /*
 ** Run DMA0 with TM0CNT as the source address, but immediately stop it.
 */
-NEW_TEST(2, 3,  {
-    if (iwram) {
-        expected[0] = 0x0016;
-        expected[1] = 0x0020;
-        expected[2] = 0x0032;
-    } else {
-        expected[0] = 0x0073;
-        expected[1] = 0x00A7;
-        expected[2] = 0x00FE;
-    }
-
+u16 TEST_02_RESULTS[TEST_KIND_MAX][3] = {
+    [TEST_KIND_IWRAM]                   = { 0x0016, 0x0020, 0x0032 },
+    [TEST_KIND_EWRAM]                   = { 0x005B, 0x0085, 0x00CE },
+    [TEST_KIND_ROM_WITH_PREFETCH]       = { 0x0067, 0x0098, 0x00E4 },
+    [TEST_KIND_ROM_WITHOUT_PREFETCH]    = { 0x0073, 0x00A7, 0x00FE },
+};
+NEW_TEST(2, TEST_02_RESULTS, {
     REG_TM0CNT_H = 0;
     REG_TM0CNT_L = 0;
     REG_TM0CNT_H = TIMER_START;
@@ -156,18 +159,22 @@ main(void)
     irqEnable(IRQ_VBLANK);
     VBlankIntrWait();
 
-    test_01_rom();
+    test_01_rom_without_prefetch();
+    test_01_rom_with_prefetch();
     test_01_iwram();
+    test_01_ewram();
 
-    test_02_rom();
+    test_02_rom_without_prefetch();
+    test_02_rom_with_prefetch();
     test_02_iwram();
+    test_02_ewram();
 
     printf("\n");
     printf("Total: %u/%u\n", nb_test_pass, nb_test_pass + nb_test_fail);
 
-
     while (true) {
         VBlankIntrWait();
     }
+
     return (0);
 }
